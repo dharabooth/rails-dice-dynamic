a1bb2a168dfab0d449ba5c9b104bf6ce563e8d9b
dharabooth <146215658+dharabooth@users.noreply.github.com>
2023-11-23 05:38:47 +0000

    done

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
 
+  get("/", { :controller => "dice", :action => "homepage"})
+
+  get("/dice/:number_of_dice/:number_sides", { :controller => "dice", :action => "dice_roll"})
+
 end
